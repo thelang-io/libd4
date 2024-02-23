2d788f31b3d9f5de21d33aa367ca8f9bcacb6a92
Aaron Delasy <aaron@thelang.io>
2024-02-23 17:54:39 +0200

    Complete rand test

--- test/rand-test.c
+++ test/rand-test.c
@@ -4,8 +4,19 @@
  */
 
 #include <the/rand.h>
+#include <assert.h>
+
+static int rand_int (void) {
+  unsigned char d[4];
+  RAND_bytes(d, 4);
+  return (d[0] << 24) | (d[1] << 16) | (d[2] << 8) | d[3];
+}
 
 static void test_rand_int (void) {
+  int num1 = rand_int();
+  int num2 = rand_int();
+
+  assert(num1 != num2);
 }
 
 int main (void) {
