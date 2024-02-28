a002e2a970b9cd8cb4ee919d34e54bcc80050443
Aaron Delasy <aaron@thelang.io>
2024-02-28 22:56:01 +0200

    Suppress Valgrind false positives

--- cmake/Tests.cmake
+++ cmake/Tests.cmake
@@ -4,8 +4,17 @@
 #
 
 if (CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME AND LIBTHE_BUILD_TESTS)
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--error-exitcode=255")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--errors-for-leak-kinds=all")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--leak-check=full")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--show-leak-kinds=all")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--suppressions=../valgrind.supp")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--tool=memcheck")
+  list(APPEND LIBTHE_MEMCHECK_FLAGS "--track-origins=yes")
+  list(JOIN LIBTHE_MEMCHECK_FLAGS " " LIBTHE_MEMCHECK_FLAGS)
+
   set(CTEST_MEMORYCHECK_COMMAND valgrind)
-  set(MEMORYCHECK_COMMAND_OPTIONS "--error-exitcode=255 --errors-for-leak-kinds=all --leak-check=full --show-leak-kinds=all --tool=memcheck --track-origins=yes")
+  set(MEMORYCHECK_COMMAND_OPTIONS "${LIBTHE_MEMCHECK_FLAGS}")
 
   include(CTest)
   set(tests any bool byte char crypto enum error number rand safe ssl)
