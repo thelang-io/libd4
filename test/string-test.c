f5f8c0ff39596850d76018daaa61b692c10d46af
Aaron Delasy <aaron@thelang.io>
2024-02-29 18:40:32 +0200

    Add memtest check file and remove suppression file

---
+++ test/string-test.c
@@ -0,0 +1,18 @@
+/*!
+ * Copyright (c) Aaron Delasy
+ * Licensed under the MIT License
+ */
+
+#include "../src/string.h"
+#include <assert.h>
+
+static void test_string_snwprintf (void) {
+}
+
+static void test_string_vsnwprintf (void) {
+}
+
+int main (void) {
+  test_string_snwprintf();
+  test_string_vsnwprintf();
+}
