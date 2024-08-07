9614c39d4d7b89ef6fa695f8a7524b24bdb48ee7
Aaron Delasy <aaron@thelang.io>
2024-08-07 21:03:50 +0300

    Finish documenting the map object

--- include/d4/map-macro.h
+++ include/d4/map-macro.h
@@ -46,58 +46,144 @@
     size_t len; \
   } d4_map_##key_type_name##MS##value_type_name##ME_t; \
   \
-  /* todo */ \
+  /**
+   * Allocates map object.
+   * @param len Number of key pairs that are passed as arguments.
+   * @param ... Arguments list that consists of pairs with key followed by value.
+   * @return Allocated map object.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t d4_map_##key_type_name##MS##value_type_name##ME_alloc (size_t len, ...); \
   \
-  /* todo */ \
+  /**
+   * Removes all elements and changes length to zero without affecting capacity.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_clear (d4_map_##key_type_name##MS##value_type_name##ME_t *self); \
   \
-  /* todo */ \
+  /**
+   * Creates a copy of provided map object.
+   * @param self Map object to create copy of.
+   * @return Copy of provided map object
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t d4_map_##key_type_name##MS##value_type_name##ME_copy (const d4_map_##key_type_name##MS##value_type_name##ME_t self); \
   \
-  /* todo */ \
+  /**
+   * Checks whether map has any elements.
+   * @param self Map object to check.
+   * @return Whether map has any elements.
+   */ \
   bool d4_map_##key_type_name##MS##value_type_name##ME_empty (const d4_map_##key_type_name##MS##value_type_name##ME_t self); \
   \
-  /* todo */ \
+  /**
+   * Compares whether map object is equal to right-hand map object.
+   * @param self Map object to check.
+   * @param rhs Right-hand map object to check.
+   * @return Whether two map objects are equal.
+   */ \
   bool d4_map_##key_type_name##MS##value_type_name##ME_eq (const d4_map_##key_type_name##MS##value_type_name##ME_t self, const d4_map_##key_type_name##MS##value_type_name##ME_t rhs); \
   \
-  /* todo */ \
+  /**
+   * Deallocates map object.
+   * @param self Map object to deallocate.
+   */ \
   void d4_map_##key_type_name##MS##value_type_name##ME_free (d4_map_##key_type_name##MS##value_type_name##ME_t self); \
   \
-  /* todo */ \
+  /**
+   * Retrieves value by key and throws if key doesn’t exist.
+   * @param state Error state to perform action on.
+   * @param line Line where error appeared.
+   * @param col Line column where error appeared.
+   * @param self Map object to perform action on.
+   * @param key Key of map object pair to retrieve.
+   * @return Value of found map object pair.
+   */ \
   value_type d4_map_##key_type_name##MS##value_type_name##ME_get (d4_err_state_t *state, int line, int col, const d4_map_##key_type_name##MS##value_type_name##ME_t self, const key_type key); \
   \
-  /* todo */ \
+  /**
+   * Checks whether map object contains a pair with provided key.
+   * @param self Map object to check.
+   * @param key Key of map object pair to check.
+   * @return Whether map object contains a pair with provided key.
+   */ \
   bool d4_map_##key_type_name##MS##value_type_name##ME_has (const d4_map_##key_type_name##MS##value_type_name##ME_t self, const key_type key); \
   \
-  /* todo */ \
+  /**
+   * Returns array of map keys.
+   * @param self Map object to use.
+   * @return Array of map keys.
+   */ \
   d4_arr_##key_type_name##_t d4_map_##key_type_name##MS##value_type_name##ME_keys (const d4_map_##key_type_name##MS##value_type_name##ME_t self); \
   \
-  /* todo */ \
+  /**
+   * Merges other map’s data into self map. When iterating, if key exists it will update pair with a new value.
+   * @param self Map object to merge into.
+   * @param other Map object to merge from.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_merge (d4_map_##key_type_name##MS##value_type_name##ME_t *self, const d4_map_##key_type_name##MS##value_type_name##ME_t other); \
   \
-  /* todo */ \
+  /**
+   * Creates and places a pair inside map object.
+   * @param self Map object to place pair into.
+   * @param id Hash of the new pair.
+   * @param key Key of the new pair.
+   * @param value Value of the new pair.
+   */ \
   void d4_map_##key_type_name##MS##value_type_name##ME_place (d4_map_##key_type_name##MS##value_type_name##ME_t self, const d4_str_t id, const key_type key, const value_type value); \
   \
-  /* todo */ \
+  /**
+   * Deallocates current map object and returns a copy of another map object.
+   * @param self Map object to deallocate.
+   * @param rhs Map object to return a copy of.
+   * @return Copy of another map object.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t d4_map_##key_type_name##MS##value_type_name##ME_realloc (d4_map_##key_type_name##MS##value_type_name##ME_t self, const d4_map_##key_type_name##MS##value_type_name##ME_t rhs); \
   \
-  /* todo */ \
+  /**
+   * Removes provided key from the map object and if key doesn’t exist throws error.
+   * @param state Error state to perform action on.
+   * @param line Line where error appeared.
+   * @param col Line column where error appeared.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_remove (d4_err_state_t *state, int line, int col, d4_map_##key_type_name##MS##value_type_name##ME_t *self, const key_type search_key); \
   \
-  /* todo */ \
+  /**
+   * Reserves a room for a specified number of pairs. Does nothing if the size provided is lower than the current capacity.
+   * @param self Map object to increase capacity of.
+   * @param size New map object capacity.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_reserve (d4_map_##key_type_name##MS##value_type_name##ME_t *self, int32_t size); \
   \
-  /* todo */ \
+  /**
+   * Sets a key inside map object, if key exists - updates its value.
+   * @param self Map object to set a pair for.
+   * @param key Key of the pair.
+   * @param value Value of the pair.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_set (d4_map_##key_type_name##MS##value_type_name##ME_t *self, const key_type key, const value_type value); \
   \
-  /* todo */ \
+  /**
+   * Reduces capacity to a current map object length multiplied by 2.
+   * @param self Map object to reduce capacity of.
+   * @return Reference to itself.
+   */ \
   d4_map_##key_type_name##MS##value_type_name##ME_t *d4_map_##key_type_name##MS##value_type_name##ME_shrink (d4_map_##key_type_name##MS##value_type_name##ME_t *self); \
   \
-  /* todo */ \
+  /**
+   * Generates string representation of the map object.
+   * @param self Map object to generate string representation for.
+   * @return String representation of the map object.
+   */ \
   d4_str_t d4_map_##key_type_name##MS##value_type_name##ME_str (const d4_map_##key_type_name##MS##value_type_name##ME_t self); \
   \
-  /* todo */ \
+  /**
+   * Returns array of map values.
+   * @param self Map object to use.
+   * @return Array of map values.
+   */ \
   d4_arr_##value_type_name##_t d4_map_##key_type_name##MS##value_type_name##ME_values (const d4_map_##key_type_name##MS##value_type_name##ME_t self);
 
 #endif
